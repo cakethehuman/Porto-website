ad29e22effa9f89134dbac251243c692f891b7f7
cakethehuman <willsonputra17@gmail.com>
2026-05-07 23:23:35 +0700

    added the skill section

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html class="h-full bg-gray-900">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @vite('resources/css/app.css')
+  </head>
+  <body class="h-full">
+    <div class="min-h-full">
+    <nav class="bg-gray-800/50">
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div class="flex h-16 items-center justify-between">
+            <div class="flex items-center">
+
+            <div class="hidden md:block">
+                <div class="ml-10 flex items-baseline space-x-4">
+                <!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
+                <a href="/" aria-current="page" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Home</a>
+                <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">About</a>
+                </div>
+            </div>
+            </div>
+            <div class="-mr-2 flex md:hidden">
+            <!-- Mobile menu button -->
+            <button type="button" command="--toggle" commandfor="mobile-menu" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500">
+                <span class="absolute -inset-0.5"></span>
+                <span class="sr-only">Open main menu</span>
+                <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6 in-aria-expanded:hidden">
+                <path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+                <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6 not-in-aria-expanded:hidden">
+                <path d="M6 18 18 6M6 6l12 12" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+            </button>
+            </div>
+        </div>
+        </div>
+
+    </nav>
+
+    <main>
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            @yield('content')
+        </div>
+    </main>
+    </div>
+
+  </body>
+</html>
---
+++ resources/views/main.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+    <section class="flex justify-center">
+        <div class="flex flex-col items-center w-300 h-110 px-25 py-18 bg-gray-800 text-white border-3 border-yellow-200 rounded-2xl shadow-lg shadow-yellow-500">
+            <img src="/build/assets/img/ame.jpg" alt="Gura pfp" class="center w-42 h-42 rounded-full">
+            <h1 class="text-3xl">Hello Im cake...👋</h1>
+            <h1 class="text-xl m-1">Data Nerd</h1>
+            <div class="flex flex-row">
+                <a href="https://www.linkedin.com/" class="m-1">
+                    <img src="/build/assets/svg/linkedin-svgrepo-com.svg" class="w-7 h-7">
+                </a>
+                <a href="https://github.com/cakethehuman" class="m-1">
+                   <img src="/build/assets/svg/github-svgrepo-com.svg" class="w-7 h-7">
+                </a>
+            </div>
+        </div> 
+    </section>  
+    <!-- This if for my skill cards -->
+    <section class="py-5">
+        <div class="flex justify-center">
+            <h1 class="text-white font-bold text-3xl w-50 h-15 py-2 text-center border-2 bg-gray-800 border-yellow-300 rounded-full m-5 shadow-lg shadow-yellow-500">
+                SKILLS
+            </h1>
+        </div>
+        <div class="flex flex-row gap-20">
+            <div class="flex-col border items-center w-140 h-90 bg-gray-800 border-5 border-emerald-500 rounded-3xl shadow-xl shadow-emerald-500/50">
+                <div class="grid grid-cols-3 gap-5 my-15 place-items-center">
+                    <img src="/build/assets/svg/scikit-learn.svg" class="w-25 h-25">
+                    <img src="/build/assets/svg/tensorflow-svgrepo-com.svg" class="w-25 h-25">
+                    <img src="/build/assets/svg/pytorch-svgrepo-com.svg" class="w-25 h-25">
+                    <img src="/build/assets/svg/statsmodels-logo-v2.svg" class="w-25 h-25">
+                    <img src="/build/assets/svg/opencv-svgrepo-com.svg" class="w-25 h-25">
+                    <img src="/build/assets/svg/tableau-icon-svgrepo-com.svg" class="w-25 h-25">
+                </div>
+            </div>
+            <div class="flex-col border items-center w-140 h-90 bg-gray-800 border-5 border-emerald-500 rounded-3xl shadow-xl shadow-emerald-500/50">
+                <div class="grid grid-cols-3 gap-5 my-15 place-items-center">
+                    <img src="/build/assets/svg/scikit-learn.svg" class="w-30 h-30">
+                    <img src="/build/assets/svg/tensorflow-svgrepo-com.svg" class="w-30 h-30">
+                    <img src="/build/assets/svg/pytorch-svgrepo-com.svg" class="w-30 h-30">
+                    <img src="/build/assets/svg/statsmodels-logo-v2.svg" class="w-30 h-30">
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
